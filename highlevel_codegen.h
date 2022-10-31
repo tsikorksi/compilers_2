06760397cfc8a352e3f5c3891371b36da6d4ddd1
tsikorksi <tsikors2@jh.edu>
2022-10-30 23:37:40 -0400

    added all notes for project

--- highlevel_codegen.h
+++ highlevel_codegen.h
@@ -9,35 +9,38 @@
 // a function definition AST node.
 class HighLevelCodegen : public ASTVisitor {
 private:
-  int m_next_label_num;
-  std::string m_return_label_name; // name of the label that return instructions should target
-  std::shared_ptr<InstructionSequence> m_hl_iseq;
+    int m_next_label_num;
+    std::string m_return_label_name; // name of the label that return instructions should target
+    std::shared_ptr<InstructionSequence> m_hl_iseq;
 
 public:
-  // the next_label_num controls where the next_label() member function
-  HighLevelCodegen(int next_label_num);
-  virtual ~HighLevelCodegen();
+    // the next_label_num controls where the next_label() member function
+    HighLevelCodegen(int next_label_num);
+    virtual ~HighLevelCodegen();
 
-  std::shared_ptr<InstructionSequence> get_hl_iseq() { return m_hl_iseq; }
+    std::shared_ptr<InstructionSequence> get_hl_iseq() { return m_hl_iseq; }
 
-  int get_next_label_num() const { return m_next_label_num; }
+    int get_next_label_num() const { return m_next_label_num; }
 
-  virtual void visit_function_definition(Node *n);
-  virtual void visit_expression_statement(Node *n);
-  virtual void visit_return_statement(Node *n);
-  virtual void visit_return_expression_statement(Node *n);
-  virtual void visit_while_statement(Node *n);
-  virtual void visit_do_while_statement(Node *n);
-  virtual void visit_for_statement(Node *n);
-  virtual void visit_if_statement(Node *n);
-  virtual void visit_if_else_statement(Node *n);
-  virtual void visit_binary_expression(Node *n);
-  virtual void visit_function_call_expression(Node *n);
-  virtual void visit_array_element_ref_expression(Node *n);
-  virtual void visit_variable_ref(Node *n);
-  virtual void visit_literal_value(Node *n);
+    virtual void visit_function_definition(Node *n);
+    virtual void visit_expression_statement(Node *n);
+    virtual void visit_unary_expression(Node *n);
+    virtual void visit_return_statement(Node *n);
+    virtual void visit_return_expression_statement(Node *n);
+    virtual void visit_while_statement(Node *n);
+    virtual void visit_do_while_statement(Node *n);
+    virtual void visit_for_statement(Node *n);
+    virtual void visit_if_statement(Node *n);
+    virtual void visit_if_else_statement(Node *n);
+    virtual void visit_binary_expression(Node *n);
+    virtual void visit_function_call_expression(Node *n);
+    virtual void visit_array_element_ref_expression(Node *n);
+    virtual void visit_variable_ref(Node *n);
+    virtual void visit_literal_value(Node *n);
+    virtual void visit_field_ref_expression(Node *n);
+    virtual void visit_indirect_field_ref_expression(Node *n);
 
 private:
-  std::string next_label();
-  // TODO: additional private member functions
+    std::string next_label();
+    // TODO: additional private member functions
 };
--- semantic_analysis.h
+++ semantic_analysis.h
@@ -10,36 +10,36 @@
 
 class SemanticAnalysis : public ASTVisitor {
 private:
-  SymbolTable *m_global_symtab, *m_cur_symtab;
+    SymbolTable *m_global_symtab, *m_cur_symtab;
 
 public:
-  SemanticAnalysis();
-  virtual ~SemanticAnalysis();
+    SemanticAnalysis();
+    virtual ~SemanticAnalysis();
 
-  virtual void visit_struct_type(Node *n);
-  virtual void visit_union_type(Node *n);
-  virtual void visit_variable_declaration(Node *n);
-  virtual void visit_basic_type(Node *n);
-  virtual void visit_function_definition(Node *n);
-  virtual void visit_function_declaration(Node *n);
-  virtual void visit_function_parameter(Node *n);
-  virtual void visit_statement_list(Node *n);
-  virtual void visit_struct_type_definition(Node *n);
-  virtual void visit_binary_expression(Node *n);
-  virtual void visit_unary_expression(Node *n);
-  virtual void visit_postfix_expression(Node *n);
-  virtual void visit_conditional_expression(Node *n);
-  virtual void visit_cast_expression(Node *n);
-  virtual void visit_function_call_expression(Node *n);
-  virtual void visit_field_ref_expression(Node *n);
-  virtual void visit_indirect_field_ref_expression(Node *n);
-  virtual void visit_array_element_ref_expression(Node *n);
-  virtual void visit_variable_ref(Node *n);
-  virtual void visit_literal_value(Node *n);
-  virtual void visit_return_expression_statement(Node *n);
+    virtual void visit_struct_type(Node *n);
+    virtual void visit_union_type(Node *n);
+    virtual void visit_variable_declaration(Node *n);
+    virtual void visit_basic_type(Node *n);
+    virtual void visit_function_definition(Node *n);
+    virtual void visit_function_declaration(Node *n);
+    virtual void visit_function_parameter(Node *n);
+    virtual void visit_statement_list(Node *n);
+    virtual void visit_struct_type_definition(Node *n);
+    virtual void visit_binary_expression(Node *n);
+    virtual void visit_unary_expression(Node *n);
+    virtual void visit_postfix_expression(Node *n);
+    virtual void visit_conditional_expression(Node *n);
+    virtual void visit_cast_expression(Node *n);
+    virtual void visit_function_call_expression(Node *n);
+    virtual void visit_field_ref_expression(Node *n);
+    virtual void visit_indirect_field_ref_expression(Node *n);
+    virtual void visit_array_element_ref_expression(Node *n);
+    virtual void visit_variable_ref(Node *n);
+    virtual void visit_literal_value(Node *n);
+    virtual void visit_return_expression_statement(Node *n);
 
 private:
-  // TODO: add helper functions
+    // TODO: add helper functions
 
     void enter_scope(std::string name);
 
@@ -62,6 +62,8 @@ private:
     static Node *promote_to_int(Node *n);
 
     static Node *implicit_conversion(Node *n, const std::shared_ptr<Type> &type);
+
+    SymbolTable *get_global_symtab() { return m_global_symtab; }
 };
 
 #endif // SEMANTIC_ANALYSIS_H
